8cb72462dda1ccf2aedb5f6f70d5d38f9744284e
Konstantin <hardfiveeven@mail.ru>
2023-07-28 15:54:08 +0300

    refactor naming models and issue ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import asyncio
+import contextlib
 from asyncio import AbstractEventLoop
-from typing import Generator, Any, AsyncGenerator
+from collections.abc import AsyncGenerator, Generator
+from typing import Any
 
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -8,7 +10,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def event_loop() -> Generator[AbstractEventLoop, Any, None]:
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
@@ -16,12 +18,11 @@ def event_loop() -> Generator[AbstractEventLoop, Any, None]:
 
 
 # TODO: replace with async context manager
-@pytest.fixture(scope='function')
+@pytest.fixture()
 async def session() -> AsyncGenerator[AsyncSession, None]:
     f = get_async_session()
     session = await f.asend(None)
     yield session
-    try:
+    with contextlib.suppress(StopAsyncIteration):
         await f.asend(None)
-    except StopAsyncIteration:
-        pass
+
