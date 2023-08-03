46d60fdb4243b4ecb24d028e887d16290048a6d7
Alexey <kuzenkovag@yandex.ru>
2023-08-03 13:28:04 +0600

    rebase

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,42 @@
 import asyncio
-import contextlib
-from asyncio import AbstractEventLoop
 from collections.abc import AsyncGenerator, Generator
 from typing import Any
 
 import pytest
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from src.database import get_async_session
+from src.config import DATABASE_URL
+from src.database import Base, async_session_maker
 from src.main import app
+
+engine = create_async_engine(DATABASE_URL, poolclass=NullPool)
 
 
 @pytest.fixture(scope="session")
-def event_loop() -> Generator[AbstractEventLoop, Any, None]:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def _prepare_database() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Generator[asyncio.AbstractEventLoop, Any, None]:
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
 
-@pytest.fixture()
-async def session() -> AsyncGenerator[AsyncSession, None]:
-    f = get_async_session()
-    session = await f.asend(None)
-    yield session
-    with contextlib.suppress(StopAsyncIteration):
-        await f.asend(None)
-
-
 @pytest.fixture(scope="session")
 async def async_client() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
-        yield ac
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
