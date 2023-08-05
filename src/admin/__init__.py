54cd0fedf66143c1c7bfc1b72ad716af43fef477
dkushlevich <dkushlevich@gmail.com>
2023-08-05 14:24:24 +0800

    Fix CustomAdmin methods

--- src/admin/__init__.py
+++ src/admin/__init__.py
@@ -9,12 +9,16 @@ from starlette_admin import DropDown, I18nConfig
 from starlette_admin._types import RequestAction
 from starlette_admin.contrib.sqla import Admin
 from starlette_admin.exceptions import FormValidationError
+from starlette_admin.views import BaseModelView
 
 from src.admin.auth_provider import EmailAndPasswordProvider
 from src.admin.utils import get_password_hash
 from src.admin.views import (
-    BlackListUserView, MatchView, UserAuthView,
-    UserQuestionnaireView, UserSettingsView,
+    BlackListUserView,
+    MatchView,
+    UserAuthView,
+    UserQuestionnaireView,
+    UserSettingsView,
 )
 from src.auth.models import AuthUser, UserSettings
 from src.config import SECRET_AUTH
@@ -24,29 +28,30 @@ from src.questionnaire.models import BlackListUser, UserQuestionnaire
 
 
 class CustomAdmin(Admin):
-    async def _render_create(self, request: Request) -> Response:
-        request.state.action = RequestAction.CREATE
-        identity = request.path_params.get("identity")
-        model = self._find_model_from_identity(identity)
-        if not model.is_accessible(request) or not model.can_create(request):
-            raise HTTPException(status.HTTP_403_FORBIDDEN)
-        if request.method == "GET":
-            return self.templates.TemplateResponse(
-                model.create_template,
-                {"request": request, "model": model},
-            )
+
+
+    async def render_form_response(self, request: Request, model: BaseModelView, action: RequestAction):
+        action_template = (
+            model.create_template if action == RequestAction.CREATE else model.edit_template
+        )
         form = await request.form()
         dict_obj = await self.form_to_dict(
-            request, form, model, RequestAction.CREATE,
+            request, form, model, action,
         )
+
         if "hashed_password" in dict_obj:
             password = dict_obj.pop("hashed_password")
             dict_obj["hashed_password"] = get_password_hash(password)
+
         try:
-            obj = await model.create(request, dict_obj)
+            if action == RequestAction.CREATE:
+                obj = await model.create(request, dict_obj)
+            else:
+                pk = request.path_params.get("pk")
+                obj = await model.edit(request, pk, dict_obj)
         except FormValidationError as exc:
             return self.templates.TemplateResponse(
-                model.create_template,
+                action_template,
                 {
                     "request": request,
                     "model": model,
@@ -57,13 +62,13 @@ class CustomAdmin(Admin):
             )
         except IntegrityError:
             return self.templates.TemplateResponse(
-                model.create_template,
+                action_template,
                 {
                     "request": request,
                     "model": model,
                     "errors": {
                         "email": (
-                                    "Пользователь с такими "
+                                    "Пользователь c такими "
                                     "данными уже существует"
                                 ),
                     },
@@ -75,24 +80,52 @@ class CustomAdmin(Admin):
         url = request.url_for(
             self.route_name + ":list", identity=model.identity,
         )
+
         if form.get("_continue_editing", None) is not None:
             url = request.url_for(
                 self.route_name + ":edit", identity=model.identity, pk=pk,
             )
         elif form.get("_add_another", None) is not None:
-            url = request.url
+            if action == RequestAction.CREATE:
+                url = request.url
+            else:
+                url = request.url_for(
+                self.route_name + ":create", identity=model.identity,
+            )
         return RedirectResponse(url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+    async def _render_create(self, request: Request) -> Response:
+        request.state.action = RequestAction.CREATE
+        identity = request.path_params.get("identity")
+        model = self._find_model_from_identity(identity)
+
+        if not model.is_accessible(request) or not model.can_create(request):
+            raise HTTPException(status.HTTP_403_FORBIDDEN)
+
+        if request.method == "GET":
+            return self.templates.TemplateResponse(
+                model.create_template,
+                {"request": request, "model": model},
+            )
+
+        return await self.render_form_response(request, model, RequestAction.CREATE)
+
 
     async def _render_edit(self, request: Request) -> Response:
         request.state.action = RequestAction.EDIT
         identity = request.path_params.get("identity")
         model = self._find_model_from_identity(identity)
+
         if not model.is_accessible(request) or not model.can_edit(request):
             raise HTTPException(status.HTTP_403_FORBIDDEN)
+
         pk = request.path_params.get("pk")
         obj = await model.find_by_pk(request, pk)
+
         if obj is None:
             raise HTTPException(status.HTTP_404_NOT_FOUND)
+
         if request.method == "GET":
             return self.templates.TemplateResponse(
                 model.edit_template,
@@ -105,52 +138,8 @@ class CustomAdmin(Admin):
                     ),
                 },
             )
-        form = await request.form()
-        dict_obj = await self.form_to_dict(
-            request, form, model, RequestAction.EDIT,
-        )
-        if "hashed_password" in dict_obj:
-            password = dict_obj.pop("hashed_password")
-            dict_obj["hashed_password"] = get_password_hash(password)
-        try:
-            obj = await model.edit(request, pk, dict_obj)
-        except FormValidationError as exc:
-            return self.templates.TemplateResponse(
-                model.edit_template,
-                {
-                    "request": request,
-                    "model": model,
-                    "errors": exc.errors,
-                    "obj": dict_obj,
-                },
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            )
-        except IntegrityError:
-            return self.templates.TemplateResponse(
-                model.create_template,
-                {
-                    "request": request,
-                    "model": model,
-                    "errors": {
-                        "email": "Пользователь с такими данными уже существует",
-                    },
-                    "obj": dict_obj,
-                },
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            )
-        pk = getattr(obj, model.pk_attr)
-        url = request.url_for(
-            self.route_name + ":list", identity=model.identity,
-        )
-        if form.get("_continue_editing", None) is not None:
-            url = request.url_for(
-                self.route_name + ":edit", identity=model.identity, pk=pk,
-            )
-        elif form.get("_add_another", None) is not None:
-            url = request.url_for(
-                self.route_name + ":create", identity=model.identity,
-            )
-        return RedirectResponse(url, status_code=status.HTTP_303_SEE_OTHER)
+
+        return await self.render_form_response(request, model, RequestAction.EDIT)
 
 
 admin = CustomAdmin(
@@ -209,10 +198,6 @@ admin.add_view(
                 Match,
                 label="Совпадения",
             ),
-            # UserLikeView(
-            #     UserLike,
-            #     label="Лайки"
-            # )
         ],
     ),
 )
