19ae42a5ace6bdcc1d5eb4ccf7160f984374a6d2
fersus <deriabin_85@mail.ru>
2023-08-07 18:00:36 +0300

    Fix status_code comparison, use fastapi.status

--- tests/auth_test/test_auth.py
+++ tests/auth_test/test_auth.py
@@ -1,11 +1,12 @@
-from dirty_equals import Contains, IsFalseLike, IsInt, IsTrueLike
+from dirty_equals import Contains, IsFalseLike, IsTrueLike
+from fastapi import status
 from httpx import AsyncClient
 
 
 async def test_register(async_client: AsyncClient):
     response = await async_client.post("/api/v1/auth/register", json={
-        "email": "test_user@mail.com", "password": "test"})
-    assert response.status_code == IsInt(exactly=201)
+        "email": "test_user23@mail.com", "password": "test"})
+    assert response.status_code == status.HTTP_201_CREATED
 
 
 async def test_login(async_client: AsyncClient):
@@ -13,7 +14,7 @@ async def test_login(async_client: AsyncClient):
                                        data={"username": "test_user@mail.com",
                                              "password": "test"})
     token = response.cookies.get("fastapiusersauth")
-    assert response.status_code == IsInt(exactly=204)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
     assert token == IsTrueLike
 
 
@@ -24,11 +25,11 @@ async def test_login_bad_credentials(async_client: AsyncClient):
     token = response.cookies.get("fastapiusersauth")
 
     assert response.text == Contains('{"detail":"LOGIN_BAD_CREDENTIALS"}')
-    assert response.status_code == IsInt(exactly=400)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert token == IsFalseLike
 
 
 async def test_logout_unauthorized(async_client: AsyncClient):
     response = await async_client.post("/api/v1/auth/logout")
     assert response.text == Contains('{"detail":"Unauthorized"}')
-    assert response.status_code == IsInt(exactly=401)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
