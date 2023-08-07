df3eb96302c20deded34ef9753f078b29f553c17
fersus <deriabin_85@mail.ru>
2023-08-07 16:17:15 +0300

    Add tests for register, login

--- tests/auth_test/test_auth.py
+++ tests/auth_test/test_auth.py
@@ -1,16 +1,34 @@
-from fastapi.testclient import TestClient
-
-from src.main import app
-
-client = TestClient(app)
-test_user = {"username": "user@example.com", "password": "string"}
+from dirty_equals import Contains, IsFalseLike, IsInt, IsTrueLike
+from httpx import AsyncClient
 
 
-def test_login():
-    response = client.post("/api/v1/auth/login", data=test_user)
+async def test_register(async_client: AsyncClient):
+    response = await async_client.post("/api/v1/auth/register", json={
+        "email": "test_user@mail.com", "password": "test"})
+    assert response.status_code == IsInt(exactly=201)
 
-    assert response.status_code == 204
 
+async def test_login(async_client: AsyncClient):
+    response = await async_client.post("/api/v1/auth/login",
+                                       data={"username": "test_user@mail.com",
+                                             "password": "test"})
+    token = response.cookies.get("fastapiusersauth")
+    assert response.status_code == IsInt(exactly=204)
+    assert token == IsTrueLike
+
+
+async def test_login_bad_credentials(async_client: AsyncClient):
+    response = await async_client.post("/api/v1/auth/login",
+                                       data={"username": "test@m.com",
+                                             "password": "string"})
     token = response.cookies.get("fastapiusersauth")
 
-    assert token is not None
+    assert response.text == Contains('{"detail":"LOGIN_BAD_CREDENTIALS"}')
+    assert response.status_code == IsInt(exactly=400)
+    assert token == IsFalseLike
+
+
+async def test_logout_unauthorized(async_client: AsyncClient):
+    response = await async_client.post("/api/v1/auth/logout")
+    assert response.text == Contains('{"detail":"Unauthorized"}')
+    assert response.status_code == IsInt(exactly=401)
