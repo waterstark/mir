595760f2436f8ffb265cc22932d9271b2aa9ea56
Konstantin <hardfiveeven@mail.ru>
2023-09-18 18:30:04 +0300

    changed config.py

--- src/config.py
+++ src/config.py
@@ -3,15 +3,15 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     DB_HOST: str
-    TEST_DB_HOST: str
+
     DB_PORT: str
-    TEST_DB_PORT: str
+
     DB_NAME: str
-    TEST_DB_NAME: str
+
     DB_USER: str
-    TEST_DB_USER: str
+
     DB_PASS: str
-    TEST_DB_PASS: str
+
     SECRET_KEY: str
 
     @property
@@ -20,14 +20,6 @@ class Settings(BaseSettings):
         return (
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@"
             f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        )
-
-    @property
-    def test_db_url(self) -> str:
-        """Product db url."""
-        return (
-            f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@"
-            f"{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
         )
 
     class Config:
