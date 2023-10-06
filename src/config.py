ea9c49b4733d22e86144bb8ed9256f7f50d96214
Konstantin <hardfiveeven@mail.ru>
2023-10-06 11:35:19 +0300

    refactoring some issue and fix url db

--- src/config.py
+++ src/config.py
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
         """Product db url."""
         return (
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}"
-            f"@{self.DB_HOST}:{self.DB_PORT}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
 
     @property
