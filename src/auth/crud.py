9916ed9c209e924783a1abc233e53af761255f4c
VladNevgod <vladnewgod@gmail.com>
2023-11-26 14:44:55 +0300

    Edit auth\crud

--- src/auth/crud.py
+++ src/auth/crud.py
@@ -1,3 +1,5 @@
+from fastapi import Depends, FastAPI, Header, HTTPException
+
 from sqlalchemy import select, update
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,7 +7,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.auth import schemas
 from src.auth.models import AuthUser, UserSettings
 from src.auth.schemas import UserCreateInput
+from fastapi.security import OAuth2PasswordBearer
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_token_header(token: str = Header(...)):
+    if token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="Invalid token provided")
+    return token
 
 async def add_user(user: UserCreateInput, session: AsyncSession):
     stmt = (
@@ -26,9 +35,9 @@ async def add_user(user: UserCreateInput, session: AsyncSession):
 
 async def get_user_profile(
     user: AuthUser,
-    session: AsyncSession,
-) -> schemas.UserProfile:
+    session: AsyncSession,) -> schemas.UserProfile:
     """Get user profile."""
+    print(AuthUser.email)
     stmt = select(UserSettings).filter_by(user_id=user.id)
     profile = await session.execute(stmt)
     return schemas.UserProfile.validate(profile.scalars().first())
