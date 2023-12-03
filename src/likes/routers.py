2009c8738833cbea2e045e3cc96dfb3189806221
Maki Nihsikino <litsonn@mail.ru>
2023-12-03 14:22:45 +0300

    Add likes auth. Add likes tests. Fix matches. Fix test db. Add after likes match creation.

--- src/likes/routers.py
+++ src/likes/routers.py
@@ -2,10 +2,14 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.background import BackgroundTasks
 
+from src.auth.base_config import current_user
+from src.auth.models import AuthUser
 from src.database import get_async_session
 from src.likes.crud import add_like
 from src.likes.schemas import UserLikeRequest, UserLikeResponse
+from src.matches.utils import create_match_after_like
 
 likes_router = APIRouter(
     prefix="/likes",
@@ -13,22 +17,25 @@ likes_router = APIRouter(
 )
 
 
-# TODO: add dependency to get UserLike from token + body when jwt is ready
 @likes_router.post(
     "",
     status_code=status.HTTP_201_CREATED,
     response_model=UserLikeResponse,
 )
 async def like_user(
+    user: Annotated[AuthUser, Depends(current_user)],
     user_like: UserLikeRequest,
     session: Annotated[AsyncSession, Depends(get_async_session)],
+    bg_tasks: BackgroundTasks,
 ):
-    like = await add_like(user_like, session)
+    like = await add_like(user, user_like, session)
 
     if like is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="bad user id",
         )
+
+    bg_tasks.add_task(create_match_after_like, session, user.id, user_like.liked_user_id)
 
     return UserLikeResponse.from_orm(like)
