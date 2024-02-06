b07385b52cf7350aa77880c89d26575cff0dcedd
pe4kahd <145473954+pe4kahd@users.noreply.github.com>
2024-02-06 17:34:59 +0300

    zxc

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -142,7 +142,7 @@ class TestAcceptance:
         )
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
-        """Проверка анкет первым пользователем, проверка превышения количества проверок анкет"""
+        """Проверка анкет первым пользователем."""
 
         response = await async_client.get(
             "/api/v1/questionnaire/list/0",
