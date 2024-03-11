c65feab2f3127ae37191dea1aa01d5c92433e61c
pe4kahd <pe4kahd@gmail.com>
2024-03-11 22:03:46 +0300

    test problemssssgtdhs

--- tests/with_db/test_likes.py
+++ tests/with_db/test_likes.py
@@ -12,33 +12,6 @@ from src.questionnaire.crud import get_rate
 from src.questionnaire.models import UserQuestionnaire
 
 
-async def test_match_creation_after_double_like(
-    async_client: AsyncClient,
-    authorised_cookie_user2: dict,
-    user2: AuthUser,
-    authorised_cookie_user3: dict,
-    questionary: UserQuestionnaire,
-    user3: AuthUser,
-    get_async_session: AsyncSession,
-    questionary_user3: UserQuestionnaire,
-):
-    """Проверка корректного создания match после взаимного лайка двух пользователей.
-    """
-    data1 = {"liked_user_id": str(user2.id), "is_liked": True}
-    data2 = {"liked_user_id": str(user3.id), "is_liked": True}
-
-    response1: Response = await async_client.post("/api/v1/likes", json=data1, cookies=authorised_cookie_user3)
-    response2: Response = await async_client.post("/api/v1/likes", json=data2, cookies=authorised_cookie_user2)
-
-    assert response1.status_code == status.HTTP_201_CREATED
-    assert response2.status_code == status.HTTP_201_CREATED
-
-    await asyncio.sleep(0.05)
-    match = await get_match_by_user_ids(get_async_session, user1_id=user3.id, user2_id=user2.id)
-
-    assert match is not None
-
-
 async def test_like_user_without_token(
     async_client: AsyncClient,
     user2: AuthUser,
