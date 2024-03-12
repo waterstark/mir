f934cce4653269a937997240eac50dc87eb911a0
pe4kahd <pe4kahd@gmail.com>
2024-03-12 21:47:17 +0300

    zxc

--- tests/with_db/test_likes.py
+++ tests/with_db/test_likes.py
@@ -1,365 +1,133 @@
-from async_asgi_testclient import TestClient
-from dirty_equals import IsUUID
+import asyncio
+import uuid
+
+from dirty_equals import IsDatetime, IsUUID
 from fastapi import status
+from httpx import AsyncClient, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.models import AuthUser
-from src.likes.models import UserLike
-from src.questionnaire.crud import get_questionnaire
+from src.matches.crud import get_match_by_user_ids
+from src.questionnaire.crud import get_rate
 from src.questionnaire.models import UserQuestionnaire
 
 
-async def test_create_questionnaire(
-    async_client: TestClient,
-    user: AuthUser,
+async def test_like_user(
+    async_client: AsyncClient,
     authorised_cookie: dict,
+    user3: AuthUser,
+    questionary_user3: UserQuestionnaire,
 ):
-    questionnaire_data = {
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2004-02-14",
-    }
-    response = await async_client.post(
-        "/api/v1/questionnaire",
-        json=questionnaire_data,
-        cookies=authorised_cookie,
-    )
+    """Проверка корректного выставления лайка.
+    """
+
+    data = {"liked_user_id": str(user3.id), "is_liked": True}
+
+    response: Response = await async_client.post("/api/v1/likes", json=data, cookies=authorised_cookie)
+
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json() == {
         "id": IsUUID,
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2004-02-14",
-        "user_id": IsUUID,
+        "liked_user_id": str(user3.id),
+        "created_at": IsDatetime(iso_string=True),
+        "is_liked": True,
     }
-    assert response.json()["user_id"] == str(user.id)
 
-
-async def test_create_questionnaire_bad_credentials(
-    async_client: TestClient,
-    user: AuthUser,
+async def test_skip_user(
+    async_client: AsyncClient,
     authorised_cookie: dict,
-):
-    questionnaire_data = {
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2004-02-14",
-    }
-    response = await async_client.post(
-        "/api/v1/questionnaire",
-        json=questionnaire_data,
-        cookies=authorised_cookie,
-    )
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    questionary = await async_client.get(
-        "/api/v1/questionnaire/get_my_quest",
-        cookies=authorised_cookie,
-    )
-    assert (
-        response.json()["detail"]
-        == f"Объект уже существует в базе данных!!!{questionary.json()['firstname']}"
-    )
-
-
-async def test_get_quest_authenticated_user(
-    async_client: TestClient,
-    user: AuthUser,
-    authorised_cookie: dict,
-):
-    response = await async_client.get(
-        "/api/v1/questionnaire/get_my_quest",
-        cookies=authorised_cookie,
-    )
-
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2004-02-14",
-        "user_id": IsUUID,
-        "id": IsUUID,
-    }
-
-
-async def test_logic_for_reusing_questionnaires(
-    async_client: TestClient,
     user2: AuthUser,
-    authorised_cookie_user2: dict,
-    like3: UserLike,
     questionary: UserQuestionnaire,
+):
+    """Проверка корректного скипа.
+    """
+
+    data = {"liked_user_id": str(user2.id), "is_liked": False}
+
+    response: Response = await async_client.post("/api/v1/likes", json=data, cookies=authorised_cookie)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json() == {
+        "id": IsUUID,
+        "liked_user_id": str(user2.id),
+        "created_at": IsDatetime(iso_string=True),
+        "is_liked": False,
+    }
+
+async def test_like_wrong_user(
+    async_client: AsyncClient,
+    authorised_cookie: dict,
+    user3: AuthUser,
     questionary_user3: UserQuestionnaire,
 ):
-    response = await async_client.get(
-        "/api/v1/questionnaire/list/0",
-        cookies=authorised_cookie_user2,
-    )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == []
+    """Проверка того, что пользователь не может лайкнуть несуществующего пользователя.
+    """
+    data = {"liked_user_id": str(uuid.uuid4())}
+
+    response: Response = await async_client.post("/api/v1/likes", json=data, cookies=authorised_cookie)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json().get("detail") == "questionanire not found"
 
 
-async def test_update_quest(
-    async_client: TestClient,
-    questionary: UserQuestionnaire,
-    user2: AuthUser,
-    authorised_cookie_user2: dict,
+async def test_self_like(
+    async_client: AsyncClient,
+    authorised_cookie: dict,
+    user3: AuthUser,
+    questionary_user3: UserQuestionnaire,
 ):
-    updated_data = {
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2004-02-14",
-    }
+    """Проверка того, что пользователь не может лайкнуть себя.
+    """
+    data = {"liked_user_id": str(user3.id)}
 
-    response = await async_client.patch(
-        f"/api/v1/questionnaire/{questionary.id}",
-        json=updated_data,
-        cookies=authorised_cookie_user2,
-    )
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-        "id": IsUUID,
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2004-02-14",
-        "user_id": IsUUID,
-    }
-    assert response.json()["user_id"] == str(user2.id)
+    response: Response = await async_client.post("/api/v1/likes", json=data, cookies=authorised_cookie)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json().get("detail") == "bad user id"
 
 
-async def test_delete_quest(
-    async_client: TestClient,
-    questionary: UserQuestionnaire,
-    user2: AuthUser,
+async def test_match_creation_after_double_like(
+    async_client: AsyncClient,
     authorised_cookie_user2: dict,
+    user2: AuthUser,
+    authorised_cookie_user3: dict,
+    questionary: UserQuestionnaire,
+    user3: AuthUser,
     get_async_session: AsyncSession,
+    questionary_user3: UserQuestionnaire,
 ):
-    response = await async_client.delete(
-        f"/api/v1/questionnaire/{questionary.id}",
-        cookies=authorised_cookie_user2,
-    )
-    assert response.status_code == status.HTTP_204_NO_CONTENT
-    response_check = await get_questionnaire(
-        user_id=user2.id,
-        session=get_async_session,
-    )
-    assert response_check is None
+    """Проверка корректного создания match после взаимного лайка двух пользователей.
+    """
+    data1 = {"liked_user_id": str(user2.id), "is_liked": True}
+    data2 = {"liked_user_id": str(user3.id), "is_liked": True}
+
+    response1: Response = await async_client.post("/api/v1/likes", json=data1, cookies=authorised_cookie_user3)
+    response2: Response = await async_client.post("/api/v1/likes", json=data2, cookies=authorised_cookie_user2)
+
+    assert response1.status_code == status.HTTP_201_CREATED
+    assert response2.status_code == status.HTTP_201_CREATED
+
+    await asyncio.sleep(0.05)
+    match = await get_match_by_user_ids(get_async_session, user1_id=user3.id, user2_id=user2.id)
+
+    assert match is not None
 
 
-async def test_create_questionnaire_without_token(
-    async_client: TestClient,
-    user: AuthUser,
+async def test_like_user_without_token(
+    async_client: AsyncClient,
+    user2: AuthUser,
 ):
-    questionnaire_data = {
-        "firstname": "string",
-        "lastname": "string",
-        "gender": "Female",
-        "photo": "string",
-        "country": "string",
-        "city": "string",
-        "about": "string",
-        "hobbies": [
-            {
-                "hobby_name": "qwewasd",
-            },
-            {
-                "hobby_name": "asidpas",
-            },
-        ],
-        "height": 0,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "birthday": "2004-02-14",
-    }
+    """Проверка выставления лайка без токена или c неправильным токеном.
+    """
+    data = {"liked_user_id": str(user2.id), "is_liked": True}
 
-    """Without cookies."""
+    response: Response = await async_client.post("/api/v1/likes", json=data, cookies={})
 
-    response = await async_client.post(
-        "/api/v1/questionnaire",
-        json=questionnaire_data,
-        cookies={},
-    )
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    """Incorrect cookies."""
-
-    response = await async_client.post(
-        "/api/v1/questionnaire",
-        json=questionnaire_data,
+    response: Response = await async_client.post(
+        "/api/v1/likes",
+        json=data,
         cookies={"mir": "some.kind.of.incorrect.cookies"},
     )
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-
-async def test_get_questionnaires_without_token(
-    async_client: TestClient,
-):
-    response = await async_client.get(
-        "/api/v1/questionnaire/get_my_quest",
-        cookies={},
-    )
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-
-    response = await async_client.get(
-        "/api/v1/questionnaire/list/0",
-        cookies={},
-    )
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-
-
-async def test_update_or_delete_quest_without_token(
-    async_client: TestClient,
-    questionary: UserQuestionnaire,
-    user2: AuthUser,
-):
-    updated_data = {
-        "firstname": "string",
-        "lastname": "string",
-        "gender": "Female",
-        "photo": "string",
-        "country": "string",
-        "city": "string",
-        "about": "string",
-        "hobbies": [
-            {
-                "hobby_name": "qwewasd",
-            },
-            {
-                "hobby_name": "asidpas",
-            },
-        ],
-        "height": 0,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "birthday": "2004-02-14",
-    }
-
-    response = await async_client.patch(
-        f"/api/v1/questionnaire/{questionary.id}",
-        json=updated_data,
-        cookies={},
-    )
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-
-    response = await async_client.delete(
-        f"/api/v1/questionnaire/{questionary.id}",
-        cookies={},
-    )
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-
-async def test_age_validation(
-    async_client: TestClient,
-    user: AuthUser,
-    authorised_cookie: dict,
-):
-    questionnaire_data = {
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "2020-02-14",
-    }
-    response = await async_client.post(
-        "/api/v1/questionnaire",
-        json=questionnaire_data,
-        cookies=authorised_cookie,
-    )
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    questionnaire_data1 = {
-        "firstname": "Антон",
-        "lastname": "Суворов",
-        "gender": "Male",
-        "photo": "Фото",
-        "country": "Россия",
-        "city": "Питер",
-        "about": "Мужичок",
-        "hobbies": [{"hobby_name": "string"}],
-        "height": 190,
-        "sport": "He занимаюсь",
-        "alcohol": "He пью",
-        "smoking": "Курю",
-        "goals": "Дружба",
-        "birthday": "1900-02-14",
-    }
-    response = await async_client.post(
-        "/api/v1/questionnaire",
-        json=questionnaire_data1,
-        cookies=authorised_cookie,
-    )
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
